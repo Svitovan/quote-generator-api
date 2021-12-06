c12c09dfff4946333c50d5cf2288fd7c7e0aaef5
Svitovan <svitovan@outlook.com>
2021-12-06 21:56:13 +0100

    final

---
+++ script.js
@@ -0,0 +1,71 @@
+const quoteConatiner = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-qoute');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteConatiner.hidden = true;
+}
+// Hide loading
+function complete() {
+    quoteConatiner.hidden = false;
+    loader.hidden = true;
+}
+
+//Show new quote
+function newQuote() {
+    loading();
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+    //Check if Author field is blank and replace it with "Unknown"
+    if(!quote.author) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    // Check Quote length to determine styling
+    if(quote.text.length > 100) {
+        quoteText.classList.add('long-quote')
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+
+    // Set quote, hide loader
+    quoteText.textContent = quote.text;
+    complete();
+}
+
+// API
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        //Catch error here
+    }
+}
+
+// Tweet
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuotes();
+
